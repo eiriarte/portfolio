baa384971e21fb101717cb9a2f0bb13fe9b7bab0
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2019-05-30 13:13:57 +0200

    Updates

---
+++ precache-manifest.de606e4c40419a14535707c4fa4c8a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "562e296231c7ebc84c5e02a408c29943",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d495ce33f3f82c89e7cc",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1b0bc73a0fe76781581e",
+    "url": "/static/css/main.833cdc52.chunk.css"
+  },
+  {
+    "revision": "d495ce33f3f82c89e7cc",
+    "url": "/static/js/2.8b89b139.chunk.js"
+  },
+  {
+    "revision": "1b0bc73a0fe76781581e",
+    "url": "/static/js/main.9973f631.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
