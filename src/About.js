5e33c94d0fd4d7a3843455b88a1465e068027c31
Eduardo I. Iriarte Gahete <eiriarte@gmail.com>
2019-07-04 10:45:52 +0200

    Mejora de la redacción

--- src/About.js
+++ src/About.js
@@ -17,16 +17,14 @@ export default function About() {
           He trabajado tanto en empresas del sector como por libre. Para terceros y en proyectos propios.
           Toda esta experiencia me ha enseñado cómo se deben hacer las cosas y también como <em>no
           se deben</em> hacer. Desde la definición de requisitos hasta el mantenimiento.</p>
-        <p className={esClass}>Aunque tengo experiencia en muy diversas tecnologías, soy principalmente
-          programador JavaScript: ESNext, Node.js, MEAN stack, AngularJS, React… Apasionado de la tecnología y
-          el software libre. Observación: escribo esto usando un emulador de Vim para Visual Studio Code en un MacBook.</p>
+        <p className={esClass}>Soy un apasionado de las nuevas tecnologías y el software libre. Aunque tengo experiencia en muy diversas tecnologías, soy principalmente
+          programador JavaScript: ESNext, Node.js, MEAN stack, AngularJS, React…</p>
         <p className={enClass}>My name is <strong>Eduardo I. Iriarte Gahete</strong>.</p>
         <p className={enClass}>I've worked at firms and as a freelance. Both for third parties and in my own projects.
           All this experience has taught me how things should be done and how they <em>should not</em>.
           From requirements definition to maintenance.</p>
-        <p className={enClass}>Though I have experience with many diverse technologies, I am, at heart, a
-          JavaScript developer: ESNext, Node.js, MEAN stack, AngularJS, React… Passionate about technology
-          and free software. Note: I'm writing this using a Vim emulator for Visual Studio Code in a MacBook.</p>
+        <p className={enClass}>I'm passionate about high tech and free software. Though I have experience with many diverse technologies, I am, at heart, a
+          JavaScript developer: ESNext, Node.js, MEAN stack, AngularJS, React…</p>
         <ul className="list-inline mt-5 about-list">
           <li className="list-inline-item">
             <a href="https://github.com/eiriarte" className="about-btn"><FaGithub /></a>
